8307a867f93e42cd6fc6b9a4e8629388957cd2e9
Ileana-Castillo <ileanag.castillo@gmail.com>
2023-12-03 11:09:06 -0600

    feat: eliminar producto y su archivo en el directorio

--- funciones/borrar.php
+++ funciones/borrar.php
@@ -4,6 +4,24 @@
 
     $redirection = "Location: " . $GLOBALS["root_site"] . "views/menu_administrador.php";
 
+    $id_producto = $_GET['cid_producto'];
+    $genero = $_GET['gender'];
+    $tipo = $_GET['type'];
+    $nombre_imagen = $_GET['image_name'];
+
+    $ruta_destino = __DIR__ . "/../imagenes/productos/" . $genero . "/" . $tipo . "/"; 
+    $ruta_final = $ruta_destino . $nombre_imagen;
+
+    if (file_exists($ruta_final)) {
+        if (unlink($ruta_final)) {
+            $response = array("mensaje" => "Archivo eliminado.");
+            echo json_encode($response);
+        } else {
+            $response = array("mensaje" => "No se pudo eliminar el archivo.");
+            echo json_encode($response);
+        }
+    }
+
     if (isset($_GET["cid_producto"])) {
         $pconexion = abrirConexion();
         seleccionarBaseDatos($pconexion);
