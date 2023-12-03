230c62b5ccab7b2f707cb5bf6a93b57228eeaa5e
Ileana-Castillo <ileanag.castillo@gmail.com>
2023-12-03 04:01:52 -0600

    feat: eliminar producto

--- funciones/borrar.php
+++ funciones/borrar.php
@@ -2,19 +2,20 @@
     include_once("../config.inc.php");
     include_once("acceder_base_datos.php");
 
-    $redirection = "Location:".$GLOBALS["root_site"]."views/menu_administrador.php";
+    $redirection = "Location: " . $GLOBALS["root_site"] . "views/menu_administrador.php";
 
-    if (isset($_GET["id_producto"])) {
+    if (isset($_GET["cid_producto"])) {
         $pconexion = abrirConexion();
         seleccionarBaseDatos($pconexion);
-
-        $cid_producto = $_GET["id_producto"];
-
-        $cquery = "DELETE FROM product WHERE id_producto = '$cid_producto'";
-
+    
+        $cid_producto = $_GET["cid_producto"];
+    
+        $cquery = "DELETE FROM product";
+        $cquery .= " WHERE id_producto = $cid_producto";
+    
         borrarDatos($pconexion, $cquery);
         cerrarConexion($pconexion);
-    }
+    }      
 
     header($redirection);
     exit();
